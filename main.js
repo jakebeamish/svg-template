fe867099b8d1f97353ab62c3d45bb55f262f2c1b
Jake Beamish <jakebeamish@gmail.com>
2024-05-15 01:31:07 +0100

    Add a vector class

--- main.js
+++ main.js
@@ -1,53 +1,44 @@
-const title = 'Untitled';
+import { createSVG, drawLine, drawPoint, downloadSVG } from "./createSVG.js";
+import { Vector } from "./geometry.js";
+import { SVG_NAMESPACE } from "./constants.js";
 
-const SVG_NAMESPACE = 'http://www.w3.org/2000/svg';
 
-document.title = `${title}`;
+const title = 'Testing';
+
+const width = 500;
+const height = 500;
 
 const svg = createSVG({
-    width: 400,
-    height: 400,
+    width: width,
+    height: height,
+    bg: 'snow',
+    id: `${title}`
 })
 
-// const WIDTH = svg.getAttribute.baseVal.value;
-// const HEIGHT = svg.getAttribute.height;
+for (let i = 0; i < 100; i++) {
+    let vector = new Vector(
+        Math.random() * width,
+        Math.random() * height
+    )
+    drawPoint(svg, vector.x, vector.y);
+}
 
+drawLine(svg, 10, 10, 100, 100);
 
-// drawLine(svg, 10, 10, 100, 100)
+function sinFract(x, y, a, b, m) {
+    return fract(Math.sin(x * a + y * b) * m)
+}
 
-const columns = 20;
-const rows = 20;
+function fract(x) {
+    return x - Math.floor(x);
+}
 
-for (let j = 0; j < columns; j++) {
-    for (let i = 0; i < rows; i++) {
-        let x = j * (WIDTH/columns);
-        drawLine(svg, x, 10, x, 100);
+onkeydown = (event) => {
+    if (event.code === 'KeyS') {
+        downloadSVG(svg, filename());
     }
 }
 
-function createSVG({
-    width = 100,
-    height = 100,
-    bg = 'snow',
-    id = 'mySVG'
-}) {
-    const svg = document.createElementNS(SVG_NAMESPACE, 'svg')
-    svg.setAttributeNS(null, 'id', id)
-    svg.setAttributeNS(null, 'width', width)
-    svg.setAttributeNS(null, 'height', height)
-    svg.setAttributeNS(null, 'style', `background-color:${bg}`)
-    svg.setAttributeNS(null, 'viewBox', `0 0 ${width} ${height}`)
-    document.body.appendChild(svg);
-    return svg;
-}
-
-function drawLine(svg, x1, y1, x2, y2) {
-    const line = document.createElementNS(SVG_NAMESPACE, 'line')
-    line.setAttributeNS(null, 'x1', x1);
-    line.setAttributeNS(null, 'y1', y1);
-    line.setAttributeNS(null, 'x2', x2);
-    line.setAttributeNS(null, 'y2', y2);
-    line.setAttributeNS(null, 'stroke', 'black');
-    // line.setAttributeNS(null, 'stroke-width', 1);
-    svg.appendChild(line);
+function filename() {
+    return `${title}_${width}x${height}`
 }
