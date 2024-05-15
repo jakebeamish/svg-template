07fd322664818ff902405a49f03f6ae1264300b9
Jake Beamish <jakebeamish@gmail.com>
2024-05-15 02:16:03 +0100

    Random shape made of a series of lines connecting random points on a grid

--- main.js
+++ main.js
@@ -1,38 +1,47 @@
 import { createSVG, drawLine, drawPoint, downloadSVG } from "./renderer.js";
 import { Vector, Line } from "./geometry.js";
-import { fract } from "./utils.js";
-// import { SVG_NAMESPACE } from "./constants.js";
-
 
 const title = 'Testing';
 
 const width = 500;
 const height = 500;
+const bg = 'snow';
 
 const svg = createSVG({
     width,
     height,
-    bg: 'snow',
+    bg,
     id: `${title}`
 })
 
-let points = [];
-for (let i = 0; i < 3; i++) {
-    let point = new Vector(
-        Math.random() * width,
-        Math.random() * height
-    )
-    points.push(point);
+const sketch = {
+    points: [],
+    lines: []
 }
 
-for (let point of points) {
+for (let i = 0; i < 15; i++) {
+    let p = new Vector(
+        (Math.floor(Math.random() * width/100)+0.5) * 100,
+        (Math.floor(Math.random() * height/100)+0.5) * 100
+    );
+    sketch.points.push(p)
+}
+
+// sketch.points.sort((a, b) => (a.x - b.x));
+
+for (let point of sketch.points) {
     drawPoint(svg, point.x, point.y)
 }
 
-for (let i = 0; i < 3; i++) {
-    const a = points[i];
-    const b = points[(i+1) % points.length];
-    drawLine(svg, a.x, a.y, b.x, b.y);
+for (let i = 0; i < sketch.points.length - 1; i++) {
+    const a = sketch.points[i];
+    const b = sketch.points[(i+1)%sketch.points.length]
+    let line = new Line(a, b);
+    sketch.lines.push(line);
+}
+
+for (let line of sketch.lines) {
+    drawLine(svg, line.a.x, line.a.y, line.b.x, line.b.y)
 }
 
 onkeydown = (event) => {
