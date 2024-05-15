0540a72510a5d90519ca86a21125bf4bd6ca5398
Jake Beamish <jakebeamish@gmail.com>
2024-05-15 13:09:55 +0100

    Add a randomElement(array) function

--- utils.js
+++ utils.js
@@ -10,6 +10,12 @@ export function lerp(a, b, t) {
     return (1 - t) * a + t * b;
 }
 
-export function randomInteger(min, max) {
-    return Math.floor(min + Math.random() * (max - min));
+export function randomInteger(min, max, randomfn = Math.random()) {
+    return Math.floor(min + randomfn * (max - min));
+}
+
+export function randomElement(array) {
+    const length = array.length;
+    const index = randomInteger(0, length);
+    return array[index];
 }
