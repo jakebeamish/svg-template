baca61e012b42dfcad135037d9024def71b0013d
Jake Beamish <jakebeamish@gmail.com>
2024-05-15 14:29:11 +0100

    Fix random helper functions using optional prng callback

--- utils.js
+++ utils.js
@@ -18,8 +18,19 @@ export function randomInteger(min, max, randomfn = Math.random()) {
     return Math.floor(min + randomfn * (max - min));
 }
 
-export function randomElement(array) {
+export function randomElement(array, randomfn = Math.random()) {
     const length = array.length;
-    const index = randomInteger(0, length);
+    const index = randomInteger(0, length, randomfn);
     return array[index];
+}
+
+export class LCG {
+    constructor(seed = 0) {
+        this.seed = seed;
+    }
+
+    next() {
+        this.seed = (this.seed * 1664525 + 1013904223) % 4294967296;
+        return this.seed / 4294967296;
+    }
 }
